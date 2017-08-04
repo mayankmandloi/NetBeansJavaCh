564aa3c8e7a3db40639b1e1b535b0e79a7a4150b
web-bhumi <web-bhumi@webbhumi>
2017-08-04 13:08:06 +0530

    Create A file

---
+++ WebBhumi/src/Streams/CrerateAFile.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Streams;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+/**
+ *
+ * @author web-bhumi
+ */
+public class CrerateAFile {
+    public static void main(String[] args) throws Exception {
+        FileOutputStream fis = new FileOutputStream("1.txt");
+        fis.write((byte)'m');
+        fis.write((byte)'a');
+        fis.write((byte)'y');
+        fis.write((byte)'a');
+        fis.write((byte)'n');
+        fis.write((byte)'k');
+        fis.write((byte)' ');
+        fis.write((byte)'w');
+        fis.write((byte)'e');
+        fis.write((byte)'b');
+        fis.write((byte)' ');
+        fis.write((byte)'B');
+        fis.write((byte)'h');
+        fis.write((byte)'u');
+        fis.write((byte)'m');
+        fis.write((byte)'i');
+
+
+    }
+}
