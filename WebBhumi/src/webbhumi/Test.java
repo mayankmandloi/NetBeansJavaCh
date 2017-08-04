400c6d4be114b9a8c9bdeaeb193c9d0194e5afd5
web-bhumi <web-bhumi@webbhumi>
2017-08-04 12:46:11 +0530

    toString

---
+++ WebBhumi/src/webbhumi/Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webbhumi;
+
+/**
+ *
+ * @author web-bhumi
+ */
+public class Test {
+ 
+    public String toString()
+    {
+        return "I am a String";
+    }
+    public static void main(String[] args) {
+        
+    }
+}
